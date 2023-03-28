c6ca2294513646562d4e6e48b0b190008991ae57
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-03-28 23:12:11 +0300

    task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
 
+import throttle from "lodash.throttle";
+
+const STORAGE_KEY = "feedback-form-state";
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onTeatareaInput, 500));
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTeatareaInput() {
+    const formDate = {
+        email: email.value,
+        message: message.value,
+      }
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formDate));
+}
+
+function populateTextarea() {
+   const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+   if (data) {
+    email.value = data.email;
+    message.value = data.message;
+   }
+}
+populateTextarea();
