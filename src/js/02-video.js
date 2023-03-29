1d4ae5efaf1621cf84c66c0fd597d9f36151c7b0
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-03-29 11:54:15 +0300

    Task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,7 @@ import throttle from "lodash.throttle";
 const STORAGE_KEY = "videoplayer-current-time";
 
 const iframe = document.querySelector('iframe'); 
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 const onPlay = function(data) {
     localStorage.setItem(STORAGE_KEY, data.seconds);
@@ -15,11 +15,4 @@ player.on('timeupdate', throttle(onPlay, 1000));
 
 player.setCurrentTime(JSON.parse(localStorage.getItem(STORAGE_KEY)) || 0);
 
-player
-  .setColor('#d8e0ff')
-  .then(function (color) {
-    console.log('The new color value: #D8E0FF');
-  })
-  .catch(function (error) {
-    console.log('An error occurred while setting the color');
-  });
+
